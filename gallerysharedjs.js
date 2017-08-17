0768cb77c51b8a10187dd4a33e3dd9f0cc6a19c8
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-08-17 11:31:55 +1000

    Updating img links (js)

--- gallerysharedjs.js
+++ gallerysharedjs.js
@@ -3,7 +3,7 @@
 
 function images(galleryInterior, type) {
     for (var i = 0; i < galleryInterior.length; i++) {
-        var path = '<div class="interior-img-block"><img class="interior-image" src="site/img/' + type +
+        var path = '<div class="interior-img-block"><img class="interior-image" src="img/' + type +
             galleryInterior[i].number +
             '.jpg" alt="' + type +  ' photo"><div class="hidden-text"><input class="add-to-favorite" type="submit" value="Add to favorite">'
             + galleryInterior[i].text + '</div></div>';
