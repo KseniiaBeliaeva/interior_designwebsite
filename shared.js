0943cfafebd07335b0de1b55df77b780c4692416
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2017-08-20 11:32:39 +1000

    Deleted menu icon

--- shared.js
+++ shared.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     });
 
     $(document).ready(function () {
-    $('.popup-menu').on('click', function () { 
+    $('.-menu').on('click', function () { 
         $(".ul-menu").toggle();
     });
     });
